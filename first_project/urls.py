8462541e73f7a1f20f7d59447c01e9e48ed35c6a
Akshay-cita <edakkatakshay@gmail.com>
2020-04-26 23:54:12 +0530

    account urls added

--- first_project/urls.py
+++ first_project/urls.py
@@ -21,5 +21,6 @@ from django.urls import include
 urlpatterns = [
     path('',views.index,name='index'),
     path('',include('firstapp.urls')),
+    path('accounts/',include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
