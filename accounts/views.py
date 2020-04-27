e03cec3a0b0700dc082e513f40ccc2652891da9a
Akshay-cita <edakkatakshay@gmail.com>
2020-04-27 23:28:59 +0530

    created view for the form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from accounts import forms
 # Create your views here.
 def signup_view(request):
-    return render(request,"signup.html")
+    form=forms.form_view()
+    if request.method =='POST':
+        form=forms.form_view(request.POST)
+        if form.is_valid():
+            #do something here
+            print("validation success")
+            print("NAME: "+form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+
+
+
+
+
+    return render(request,"signup.html",{'form':form})
