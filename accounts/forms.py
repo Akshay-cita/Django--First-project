574bb97660d4d6c7b192e8576368043b7669e48a
Akshay-cita <edakkatakshay@gmail.com>
2020-04-28 23:21:03 +0530

    form for sign up first_name,last_name,email

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from accounts.models import User
 
 class form_view(forms.Form):
     name=forms.CharField()
     email=forms.EmailField()
     text=forms.CharField(widget=forms.Textarea)
+
+
+class NewUserform(forms.ModelForm):
+    class Meta:
+        model=User
+        fields='__all__'
