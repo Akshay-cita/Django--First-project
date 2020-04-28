e0643ee8d11e330b0e416318ada8c262c041d294
Akshay-cita <edakkatakshay@gmail.com>
2020-04-28 23:24:16 +0530

    url created

--- first_project/urls.py
+++ first_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import include
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('',include('firstapp.urls')),
+    # path('',include('firstapp.urls')),
     path('accounts/',include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
