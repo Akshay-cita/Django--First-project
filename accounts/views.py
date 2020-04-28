a0b4e30b34cb2a00afb28625d31ca253af702ed8
Akshay-cita <edakkatakshay@gmail.com>
2020-04-28 23:23:27 +0530

    user_account view created

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
-from accounts import forms
+from accounts.forms import NewUserform
 # Create your views here.
+def detailed(request):
+    return render(request,'article_detail.html')
+
 def signup_view(request):
     form=forms.form_view()
     if request.method =='POST':
@@ -13,9 +16,15 @@ def signup_view(request):
             print("NAME: "+form.cleaned_data['name'])
             print("EMAIL: "+form.cleaned_data['email'])
             print("TEXT: "+form.cleaned_data['text'])
-
-
-
-
-
     return render(request,"signup.html",{'form':form})
+
+def user_account(request):
+    form=NewUserform()
+    if request.method =='POST':
+        form=NewUserform(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return detailed(request)
+        else:
+            print("error/....")
+    return render(request,'user.html',{'form':form})
