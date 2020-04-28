89e4c054f18fbff786b3f014669f5f4b7f51c90e
Akshay-cita <edakkatakshay@gmail.com>
2020-04-28 23:22:35 +0530

    url created for the NewUserForm view

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,re_path
 from accounts import views
 
 urlpatterns=[
-path('signup/',views.signup_view,name="signup")
+path('signup/',views.signup_view,name="signup"),
+path('new/',views.user_account,name="new"),
+
 ]
