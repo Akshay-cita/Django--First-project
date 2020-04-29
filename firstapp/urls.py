68b114d5eb09d1b7a5a9b9579e6c022611149862
Akshay-cita <edakkatakshay@gmail.com>
2020-04-29 16:16:08 +0530

    Url,view,template created

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path,re_path
 from firstapp import views
 
+
+app_name='firstapp'
+
 urlpatterns=[
     path('firstapp',views.index,name='index'),
-    re_path(r'^(?P<slug>[\w-]+)/$',views.blog_detail,name='detail'),
+    re_path(r'^(?P<slug>[\w-]+)/$',views.blog_detail,name=' blog_detail'),
+    path('signup/',views.signup_view,name="signup"),
+    path('new/',views.user_account,name="new"),
 
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
-from firstapp.models import Post
+from firstapp.models import Post,User
+from firstapp.forms import NewUserform,form_view
 
 def index(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     my_dict={'posts':posts}
-    return render(request,'index.html',context=my_dict)
+    return render(request,'firstapp/index.html',context= my_dict)
 
 def blog_detail(request,slug):
     # return HttpResponse(slug)
     blog=Post.objects.get(slug=slug)
     return render(request,'article_detail.html',{'blogs':blog})
 
+def signup_view(request):
+    form=form_view()
+    if request.method =='POST':
+        form=form_view(request.POST)
+        if form.is_valid():
+            #do something here
+            print("validation success")
+            print("NAME: "+form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+    return render(request,"signup.html",{'form':form})
+
+def user_account(request):
+    form=NewUserform()
+    if request.method =='POST':
+        form=NewUserform(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("error/....")
+    return render(request,'user.html',{'form':form})
 
 
 # Create your views here.
